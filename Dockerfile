605eb47197545adb94c3ad6a991f69c87ee610ad
Johan Miranda <johanops@MacBook-Pro-de-Johan.local>
2022-12-04 14:30:59 -0500

    Add mysql connection, add dockerfile and docker compose, add config devcontainer for dockercompose.yaml

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:18
+
+WORKDIR /app
+
+COPY package*.json .
+
+COPY . .
+
+RUN npm install
+
+CMD ["node", "src/app.js"]
--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import express from "express";
 import { jwtRoutes } from "./routes/jwt.routes.js";
 import bodyParser from "body-parser";
+import mysql from "mysql2"
+
 const app = express()
+const pool = mysql.createPool({
+    host: 'db',
+    user: 'root',
+    database: 'example',
+    password: "example"
+});
+
+pool.query("SELECT 1 + 1");
+console.log(pool.query("SELECT 1"))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
